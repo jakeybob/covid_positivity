72b91e8f4f6117050836166ab1a8b4bb6b0c3b6c
Bob Taylor <j.boaby@gmail.com>
2020-11-17 16:53:55 +0000

    2020-11-17 updates

--- run_all_jobs.R
+++ run_all_jobs.R
@@ -6,9 +6,16 @@ while(.rs.invokeRpc("get_jobs")[[get_data_job_id]]$completed == 0){
   Sys.sleep(10) # check status every ten seconds
   }
 
-rstudioapi::jobRunScript("generate_plots.R")
-rstudioapi::jobRunScript("generate_maps.R")
-rstudioapi::jobRunScript("generate_phase_anim.R")
+generate_plots_job_id <- rstudioapi::jobRunScript("generate_plots.R")
+generate_maps_job_id <- rstudioapi::jobRunScript("generate_maps.R")
+generate_phase_anim_job_id <- rstudioapi::jobRunScript("generate_phase_anim.R")
+
+while(.rs.invokeRpc("get_jobs")[[generate_plots_job_id]]$completed == 0 |
+      .rs.invokeRpc("get_jobs")[[generate_maps_job_id]]$completed == 0 |
+      .rs.invokeRpc("get_jobs")[[generate_phase_anim_job_id]]$completed == 0){
+  Sys.sleep(10) # check status every ten seconds
+}
+
 rm(list = ls())
 
 # commit and push (note won't add any new files to repo)
