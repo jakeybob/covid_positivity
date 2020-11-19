2ebac01aee2771d8a4affe4fce43bf77c3c6d019
Bob Taylor <j.boaby@gmail.com>
2020-11-19 17:27:04 +0000

    2020-11-19 updates

--- run_all_jobs.R
+++ run_all_jobs.R
@@ -8,11 +8,12 @@ while(.rs.invokeRpc("get_jobs")[[get_data_job_id]]$completed == 0){
 
 generate_plots_job_id <- rstudioapi::jobRunScript("generate_plots.R")
 generate_maps_job_id <- rstudioapi::jobRunScript("generate_maps.R")
-generate_phase_anim_job_id <- rstudioapi::jobRunScript("generate_phase_anim.R")
+# generate_phase_anim_job_id <- rstudioapi::jobRunScript("generate_phase_anim.R")
 
 while(.rs.invokeRpc("get_jobs")[[generate_plots_job_id]]$completed == 0 |
-      .rs.invokeRpc("get_jobs")[[generate_maps_job_id]]$completed == 0 |
-      .rs.invokeRpc("get_jobs")[[generate_phase_anim_job_id]]$completed == 0){
+      .rs.invokeRpc("get_jobs")[[generate_maps_job_id]]$completed == 0 
+      # .rs.invokeRpc("get_jobs")[[generate_phase_anim_job_id]]$completed == 0
+      ){
   Sys.sleep(10) # check status every ten seconds
 }
 
